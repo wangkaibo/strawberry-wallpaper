b5f25cb77c4b4c963cecf5e9b0c5536a9c3ec5e8
yangpeng <aitexiaoy@163.com>
2020-02-13 21:19:28 +0800

    feat(admin): update doc

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,5 +1,17 @@
 const path = require('path')
 
 module.exports = {
-    outputDir: path.resolve(__dirname, '../static')
+    outputDir: path.resolve(__dirname, '../static'),
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.less$/,
+    //             use: [
+    //                 'vue-style-loader',
+    //                 'css-loader',
+    //                 'less-loader'
+    //             ]
+    //         },
+    //     ]
+    // }
 }
